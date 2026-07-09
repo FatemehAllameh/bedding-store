0bcaa6b540ec2269aa928ddec5036f0a3023f99f
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-07-09 15:29:08 +0330

    feat: add announcement bar

--- src/main.js
+++ src/main.js
@@ -6,13 +6,18 @@ const header = $.querySelector("#header");
 function initMenu() {
   const menuBtn = $.querySelector(".menu-btn");
   const nav = $.querySelector(".nav");
+  const closeBtn = $.querySelector(".close-btn");
+  const announcementBar = $.querySelector(".announcement-bar")
 
-  if (!menuBtn || !nav) return;
+  if (!menuBtn || !nav || !closeBtn || !announcementBar) return;
 
   menuBtn.addEventListener("click", (e) => {
     menuBtn.classList.toggle("active");
     nav.classList.toggle("open");
   });
+  closeBtn.addEventListener("click", () => {
+    announcementBar.classList.remove("md:block")
+  })
 }
 
 if (header) {
