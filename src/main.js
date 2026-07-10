a5eb3430b68df326658e5712880080971b06ff7c
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-07-10 18:07:00 +0330

    refactor: remove header component rendering

--- src/main.js
+++ src/main.js
@@ -1,31 +1,16 @@
 import "./style.css";
 
-const header = document.querySelector("#header");
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-btn");
+const announcementBar = document.querySelector(".announcement-bar");
 
-function initMenu() {
-  const menuBtn = document.querySelector(".menu-btn");
-  const nav = document.querySelector(".nav");
-  const overlay = document.querySelector(".overlay")
-  const closeBtn = document.querySelector(".close-btn");
-  const announcementBar = document.querySelector(".announcement-bar")
-
-  if (!menuBtn || !nav || !closeBtn || !announcementBar) return;
-
-  menuBtn.addEventListener("click", (e) => {
-    menuBtn.classList.toggle("active");
-    nav.classList.toggle("open");
-    overlay.classList.toggle("active")
-  });
-  closeBtn.addEventListener("click", () => {
-    announcementBar.classList.remove("md:block")
-  })
-}
-
-if (header) {
-  fetch("/src/components/header.html")
-    .then((res) => res.text())
-    .then((data) => {
-      header.innerHTML = data;
-      initMenu();
-    });
-}
+menuBtn.addEventListener("click", (e) => {
+  menuBtn.classList.toggle("active");
+  nav.classList.toggle("open");
+  overlay.classList.toggle("active");
+});
+closeBtn.addEventListener("click", () => {
+  announcementBar.classList.remove("md:block");
+});
