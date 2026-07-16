acf23308b70f48fc864947f061f1274bcdf0b9e7
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-07-16 22:13:55 +0330

    feat: add product description section

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import "./style.css";
 
+// --- header --- //
+// elements
 const menuBtn = document.querySelector(".menu-btn");
 const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close-btn");
 const announcementBar = document.querySelector(".announcement-bar");
-const filterTitle = document.querySelectorAll(".filter-title");
-const filterList = document.querySelector(".filter-list");
-const colorBtns = document.querySelectorAll(".color-btn");
-const addBtn = document.querySelector(".add-btn");
-const reduceBtn = document.querySelector(".reduce-btn");
-const quntityText = document.querySelector(".quntity-text");
-
+// events
 menuBtn.addEventListener("click", (e) => {
   menuBtn.classList.toggle("active");
   nav.classList.toggle("open");
@@ -20,14 +16,34 @@ menuBtn.addEventListener("click", (e) => {
 closeBtn.addEventListener("click", () => {
   announcementBar.classList.remove("md:block");
 });
+
+// --- products page --- //
+// elements
+const filterTitle = document.querySelectorAll(".filter-title");
+const filterList = document.querySelector(".filter-list");
+// events
 filterTitle.forEach((title) => {
   const chevronToggle = title.querySelector(".chevron-toggle");
   const list = title.nextElementSibling;
   title.addEventListener("click", () => {
+    title.classList.toggle("mb-4");
     chevronToggle.classList.toggle("rotate");
     list.classList.toggle("open");
   });
 });
+
+// --- product details page --- //
+// elements
+const colorBtns = document.querySelectorAll(".color-btn");
+const addBtn = document.querySelector(".add-btn");
+const reduceBtn = document.querySelector(".reduce-btn");
+const quntityText = document.querySelector(".quntity-text");
+const readmoreBtn = document.querySelector(".readmore-btn");
+const readlessBtn = document.querySelector(".readless-btn");
+const descriptionWrapper = document.querySelector(".description-wrapper");
+const fade = document.querySelector(".fade");
+
+// events
 colorBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     colorBtns.forEach((btn) => {
@@ -37,15 +53,32 @@ colorBtns.forEach((btn) => {
   });
 });
 
-addBtn.addEventListener("click", () => {
-  let quantity = quntityText.textContent;
-  quntityText.textContent = ++quantity;
-});
-reduceBtn.addEventListener("click", () => {
-  if (quntityText.textContent > 0) {
+if (addBtn && reduceBtn && quntityText) {
+  addBtn.addEventListener("click", () => {
     let quantity = quntityText.textContent;
-    quntityText.textContent = --quantity;
-  } else {
-    return;
-  }
+    quntityText.textContent = ++quantity;
+  });
+  reduceBtn.addEventListener("click", () => {
+    if (quntityText.textContent > 0) {
+      let quantity = quntityText.textContent;
+      quntityText.textContent = --quantity;
+    }
+  });
+}
+
+readmoreBtn.addEventListener("click", () => {
+  descriptionWrapper.classList.remove("max-h-32");
+  fade.classList.add("hidden");
+  readmoreBtn.classList.add("hidden");
+  readmoreBtn.classList.remove("flex");
+  readlessBtn.classList.remove("hidden");
+  readlessBtn.classList.add("flex");
+});
+readlessBtn.addEventListener("click", () => {
+  descriptionWrapper.classList.add("max-h-32");
+  fade.classList.remove("hidden");
+  readmoreBtn.classList.remove("hidden");
+  readmoreBtn.classList.add("flex");
+  readlessBtn.classList.add("hidden");
+  readlessBtn.classList.remove("flex");
 });
