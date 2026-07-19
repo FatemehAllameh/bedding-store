1abc5b325a339b8161985fb75fe1005e828cc8ea
FatemehAllameh <fatemeh.allameh19@gmail.com>
2026-07-19 11:13:36 +0330

    fix: fix JS bugs

--- src/main.js
+++ src/main.js
@@ -27,37 +27,38 @@ closeBtn.addEventListener("click", () => {
 const filterTitle = document.querySelectorAll(".filter-title");
 const filterList = document.querySelector(".filter-list");
 // events
-filterTitle.forEach((title) => {
-  const chevronToggle = title.querySelector(".chevron-toggle");
-  const list = title.nextElementSibling;
-  title.addEventListener("click", () => {
-    title.classList.toggle("mb-4");
-    chevronToggle.classList.toggle("rotate");
-    list.classList.toggle("open");
+if (filterTitle) {
+  filterTitle.forEach((title) => {
+    const chevronToggle = title.querySelector(".chevron-toggle");
+    const list = title.nextElementSibling;
+    title.addEventListener("click", () => {
+      title.classList.toggle("mb-4");
+      chevronToggle.classList.toggle("rotate");
+      list.classList.toggle("open");
+    });
   });
-});
+}
 
 // --- product details page --- //
 // elements
 const colorBtns = document.querySelectorAll(".color-btn");
+
+// events
+if (colorBtns) {
+  colorBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      colorBtns.forEach((btn) => {
+        btn.style.borderColor = "#FAFAFA";
+      });
+      btn.style.borderColor = btn.dataset.color;
+    });
+  });
+}
+// elements
 const addBtn = document.querySelector(".add-btn");
 const reduceBtn = document.querySelector(".reduce-btn");
 const quntityText = document.querySelector(".quntity-text");
-const readmoreBtn = document.querySelector(".readmore-btn");
-const readlessBtn = document.querySelector(".readless-btn");
-const descriptionWrapper = document.querySelector(".description-wrapper");
-const fade = document.querySelector(".fade");
-
 // events
-colorBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    colorBtns.forEach((btn) => {
-      btn.style.borderColor = "#FAFAFA";
-    });
-    btn.style.borderColor = btn.dataset.color;
-  });
-});
-
 if (addBtn && reduceBtn && quntityText) {
   addBtn.addEventListener("click", () => {
     let quantity = quntityText.textContent;
@@ -70,29 +71,39 @@ if (addBtn && reduceBtn && quntityText) {
     }
   });
 }
-
-readmoreBtn.addEventListener("click", () => {
-  descriptionWrapper.classList.remove("max-h-32");
-  fade.classList.add("hidden");
-  readmoreBtn.classList.add("hidden");
-  readmoreBtn.classList.remove("flex");
-  readlessBtn.classList.remove("hidden");
-  readlessBtn.classList.add("flex");
-});
-readlessBtn.addEventListener("click", () => {
-  descriptionWrapper.classList.add("max-h-32");
-  fade.classList.remove("hidden");
-  readmoreBtn.classList.remove("hidden");
-  readmoreBtn.classList.add("flex");
-  readlessBtn.classList.add("hidden");
-  readlessBtn.classList.remove("flex");
-});
+// elements
+const readmoreBtn = document.querySelector(".readmore-btn");
+const readlessBtn = document.querySelector(".readless-btn");
+const descriptionWrapper = document.querySelector(".description-wrapper");
+const fade = document.querySelector(".fade");
+// events
+if (readmoreBtn && readlessBtn && descriptionWrapper && fade) {
+  readmoreBtn.addEventListener("click", () => {
+    descriptionWrapper.classList.remove("max-h-32");
+    fade.classList.add("hidden");
+    readmoreBtn.classList.add("hidden");
+    readmoreBtn.classList.remove("flex");
+    readlessBtn.classList.remove("hidden");
+    readlessBtn.classList.add("flex");
+  });
+  readlessBtn.addEventListener("click", () => {
+    descriptionWrapper.classList.add("max-h-32");
+    fade.classList.remove("hidden");
+    readmoreBtn.classList.remove("hidden");
+    readmoreBtn.classList.add("flex");
+    readlessBtn.classList.add("hidden");
+    readlessBtn.classList.remove("flex");
+  });
+}
 
 // Glide
-new Glide(".glide", {
-  type: "carousel",
-  direction: "rtl",
-  perView: 1.4,
-  focusAt: "0",
-  gap: 12,
-}).mount();
+const glide = document.querySelector(".glide");
+if (glide) {
+  new Glide(".glide", {
+    type: "carousel",
+    direction: "rtl",
+    perView: 1.4,
+    focusAt: "0",
+    gap: 12,
+  }).mount();
+}
